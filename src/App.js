ed2060847915ed484f25320963d9ab483334b130
Chris Mittell <mittell@hotmail.co.uk>
2022-08-30 10:50:40 +0900

    feat: added Navbar component and styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Navbar } from './components/Navbar/Navbar';
+
 function App() {
-	return <div></div>;
+	return (
+		<>
+			<Navbar />
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { FaHamburger, FaBars, FaTimes } from 'react-icons/fa';
+import './NavbarStyles.css';
+
+export const Navbar = () => {
+	const [nav, setNav] = useState(false);
+
+	const handleNav = () => {
+		setNav(!nav);
+	};
+
+	return (
+		<div className='navbar'>
+			<div className='container'>
+				<div>
+					<FaHamburger size={40} style={{ marginLeft: '4px' }} />
+				</div>
+				<ul className={nav ? 'nav-menu active' : 'nav-menu'}>
+					<li>Home</li>
+					<li>Order</li>
+					<li>Menu</li>
+					<li>Delivery</li>
+					<li>Contact</li>
+				</ul>
+				<div className='hamburger' onClick={handleNav}>
+					{nav ? (
+						<FaTimes size={20} style={{ color: '#fff' }} />
+					) : (
+						<FaBars size={20} />
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
