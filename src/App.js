e19c2d614c061e0ce04197cc33c321a3078e7d09
Chris Mittell <mittell@hotmail.co.uk>
2022-08-30 11:31:36 +0900

    chore: removed asset

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Craft1 from './assets/craft1.jpg';
 import Craft2 from './assets/craft2.jpg';
 import Chicken from './assets/chicken.jpg';
 import Burger from './assets/burger.jpg';
-import HeroBg from './assets/hero-bg.jpg';
 
 function App() {
 	return (
