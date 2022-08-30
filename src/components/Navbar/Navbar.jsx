8b0fedb6e8ce605c6f08ba2c7b9d063c71c7128a
Chris Mittell <mittell@hotmail.co.uk>
2022-08-30 11:30:12 +0900

    feat: updated Navbar styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,13 +4,24 @@ import './NavbarStyles.css';
 
 const Navbar = () => {
 	const [nav, setNav] = useState(false);
+	const [color, setColor] = useState(false);
 
 	const handleNav = () => {
 		setNav(!nav);
 	};
 
+	const changeColor = () => {
+		if (window.scrollY >= 100) {
+			setColor(true);
+		} else {
+			setColor(false);
+		}
+	};
+
+	window.addEventListener('scroll', changeColor);
+
 	return (
-		<div className='navbar'>
+		<div className={color ? 'navbar navbar-bg' : 'navbar'}>
 			<div className='container'>
 				<div>
 					<FaHamburger size={40} style={{ marginLeft: '4px' }} />
